c794a2c67da19c0009f4ace4726cbb8b303d7956
Atiq Samtia <atiq@ioitechnologies.com>
2020-04-02 04:39:10 +0500

    Fixed Gesture Detector on Post Item Click

--- lib/widgets/post_list_item.dart
+++ lib/widgets/post_list_item.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 
 import '../model/post_entity.dart';
+import '../pages/post_details.dart';
 
 class PostListItem extends StatelessWidget {
   final PostEntity post;
@@ -11,53 +12,58 @@ class PostListItem extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 10.0),
-      child: Row(
-        children: <Widget>[
-          Padding(
-            padding: const EdgeInsets.only(right: 14.0),
-            child: ClipRRect(
-              borderRadius: BorderRadius.circular(10),
-              child: FadeInImage.assetNetwork(
-                image: post.image,
-                placeholder: 'images/placeholder.jpg',
-                width: 100,
-                height: 85,
-                fit: BoxFit.cover,
+      child: GestureDetector(
+        onTap: () {
+          Navigator.push(context, MaterialPageRoute(builder: (context) => PostDetails(post)));
+        },
+        child: Row(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.only(right: 14.0),
+              child: ClipRRect(
+                borderRadius: BorderRadius.circular(10),
+                child: FadeInImage.assetNetwork(
+                  image: post.image,
+                  placeholder: 'images/placeholder.jpg',
+                  width: 100,
+                  height: 85,
+                  fit: BoxFit.cover,
+                ),
               ),
             ),
-          ),
-          Flexible(
-            child: SizedBox(
-              height: 85.0,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: <Widget>[
-                  Text(
-                    post.title,
-                    textAlign: TextAlign.left,
-                    maxLines: 3,
-                    overflow: TextOverflow.ellipsis,
-                    style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 18.0, fontFamily: 'Roboto'),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: <Widget>[
-                      Text(post.date),
-                      Expanded(
-                        child: Text(
-                          post.category,
-                          textAlign: TextAlign.end,
-                          overflow: TextOverflow.ellipsis,
+            Flexible(
+              child: SizedBox(
+                height: 85.0,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Text(
+                      post.title,
+                      textAlign: TextAlign.left,
+                      maxLines: 3,
+                      overflow: TextOverflow.ellipsis,
+                      style: TextStyle(color: Colors.black, fontWeight: FontWeight.bold, fontSize: 18.0, fontFamily: 'Roboto'),
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: <Widget>[
+                        Text(post.date),
+                        Expanded(
+                          child: Text(
+                            post.category,
+                            textAlign: TextAlign.end,
+                            overflow: TextOverflow.ellipsis,
+                          ),
                         ),
-                      ),
-                    ],
-                  ),
-                ],
+                      ],
+                    ),
+                  ],
+                ),
               ),
-            ),
-          )
-        ],
+            )
+          ],
+        ),
       ),
     );
   }
