0e186792ea12098623a321bf831037351af5e77a
Atiq Samtia <atiq@ioitechnologies.com>
2020-04-02 08:52:22 +0500

    Change Base URL in Config

--- lib/config.dart
+++ lib/config.dart
@@ -1,22 +1,9 @@
 import 'package:flutter/material.dart';
 
-const String TITLE = "ProPakistani";
+const String TITLE = "Tech Insanity";
 
 /// last slash ' / ' is mandatory
-const String URL = "https://propakistani.pk/";
+const String URL = "https://techinsanity.com/";
 
-const int FEATURED_CATEGORY_ID = 46671;
+const int FEATURED_CATEGORY_ID = 20;
 const String FEATURED_CATEGORY_TITLE = 'Featured';
-
-const MaterialColor APP_COLOR = MaterialColor(0xFF249991, {
-  50: Color(0x1A249991),
-  100: Color(0x33249991),
-  200: Color(0x4D249991),
-  300: Color(0x66249991),
-  400: Color(0x80249991),
-  500: Color(0x99249991),
-  600: Color(0xB3249991),
-  700: Color(0xCC249991),
-  800: Color(0xE6249991),
-  900: Color(0xFF249991),
-});
